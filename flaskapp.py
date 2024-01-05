9e8cfd15f72455cd4bd1fae5827a43e3ba123eef
Mayank Sharma <mayank2001sharma@gmail.com>
2024-01-05 20:09:17 +0530

    First Commit

---
+++ flaskapp.py
@@ -0,0 +1,72 @@
+import os
+
+import cv2
+from flask import Flask, render_template, Response, session
+from flask_wtf import FlaskForm
+from werkzeug.utils import secure_filename
+from wtforms import FileField, SubmitField
+from wtforms.validators import InputRequired
+
+from YOLO_Video import video_detection
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'YOUR SECRET KEY'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+
+class UploadFileForm(FlaskForm):
+
+    file = FileField("File",validators=[InputRequired()])
+    submit = SubmitField("Run")
+
+
+def generate_frames(path_x = ''):
+    yolo_output = video_detection(path_x)
+    for detection_ in yolo_output:
+        ref,buffer=cv2.imencode('.jpg',detection_)
+
+        frame=buffer.tobytes()
+        yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame +b'\r\n')
+
+def generate_frames_web(path_x):
+    yolo_output = video_detection(path_x)
+    for detection_ in yolo_output:
+        ref,buffer=cv2.imencode('.jpg',detection_)
+
+        frame=buffer.tobytes()
+        yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame +b'\r\n')
+
+@app.route('/', methods=['GET','POST'])
+@app.route('/home', methods=['GET','POST'])
+def home():
+    session.clear()
+    return render_template('indexproject.html')
+
+@app.route("/webcam", methods=['GET','POST'])
+
+def webcam():
+    session.clear()
+    return render_template('ui.html')
+@app.route('/FrontPage', methods=['GET','POST'])
+def front():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
+                               secure_filename(file.filename)))  # Then save the file
+        session['video_path'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
+                                             secure_filename(file.filename))
+    return render_template('videoprojectnew.html', form=form)
+@app.route('/video')
+def video():
+    return Response(generate_frames(path_x = session.get('video_path', None)),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/webapp')
+def webapp():
+    return Response(generate_frames_web(path_x=0), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True)
